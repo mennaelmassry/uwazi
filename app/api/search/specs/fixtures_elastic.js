b36164aa898961b183ce97c3ee6ace907f3747ef
Federico Nocetti <fnocetti@users.noreply.github.com>
2021-07-07 10:36:20 -0300

    Fix endpoint

--- app/api/search/specs/fixtures_elastic.js
+++ app/api/search/specs/fixtures_elastic.js
@@ -214,6 +214,17 @@ export const fixtures = {
         ],
       },
     },
+    // permissions
+    {
+      _id: db.id(),
+      sharedId: 'with permissions but not published',
+      template,
+      language: 'es',
+      title: 'with permissions but not published',
+      published: false,
+      permissions: [{ refId: 'collabId', type: 'user', level: 'read' }],
+    },
+    //metadata filters
     {
       _id: db.id(),
       sharedId: 'inherited 2',
